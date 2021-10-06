2d906c2202b9b57acc2016a93847e4d0f989d144
Philippe Coval <philippe.coval@huawei.com>
2021-10-06 21:22:00 +0200

    drivers: Add dummy driver as fallback
    This can help porting to new platforms
    
    Change-Id: Icbc3536941116703b8797d140b21c0c80641eb6b
    Relate-to: https://git.ostc-eu.org/OSTC/requirements/-/issues/15
    Signed-off-by: Philippe Coval <philippe.coval@huawei.com>

---
+++ src/drivers/dummy.c
@@ -0,0 +1,12 @@
+// -*- mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+// vim: ft=cpp:expandtab:ts=4:sw=4:softtabstop=4:
+// SPDX-FileCopyrightText: Huawei Inc.
+// SPDX-License-Identifier: MIT
+
+#include <stdio.h>
+#warning "warning: no driver selected"
+
+void hal_init(void)
+{
+    printf("warning: no driver built\n");
+}
